df905e3f847705825ee953eb41313a12a46ab6e7
Mobtrucky <128086337+mobtrucky@users.noreply.github.com>
2023-05-14 18:04:46 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -8,16 +8,17 @@ enum RadioMessage {
 radio.setGroup(5)
 // Número que indentifica o Micro:Bit.
 basic.showNumber(4)
+// Envia a cor que o sensor RGB detecta.
 basic.forever(function () {
-    // Detectação de qual cor o sensor RGB esta vendo no 4. Nesse caso, vai detectar a cor preta. Com o resultado final de branco no 4 e preto no 3.
+    // Detectação de qual cor o sensor RGB esta vendo no 4. Nesse caso, vai detectar a cor branco.
     if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 36 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) < 85 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) == 255)) {
         radio.sendMessage(RadioMessage.branco)
     }
-    // Detectação de qual cor o sensor RGB esta vendo no 4. Nesse caso, vai detectar a cor preta. Com o resultado final de preto no 4 e preto no 3.
+    // Detectação de qual cor o sensor RGB esta vendo no 4. Nesse caso, vai detectar a cor preta.
     if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 36 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) < 85 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) == 255)) {
         radio.sendMessage(RadioMessage.preto)
     }
-    // Preto e verde
+    // Detectação de qual cor o sensor RGB esta vendo no 4. Nesse caso, vai detectar a cor verde.
     if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 57 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) == 255 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) < 38)) {
         radio.sendMessage(RadioMessage.verde)
     }
