911e255f5195d6ebdb6b7c4a4d2f1b5b0922f86c
Mobtrucky <128086337+mobtrucky@users.noreply.github.com>
2023-05-17 17:01:37 -0300

    Atualizar 'pxt.json, main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -4,79 +4,33 @@ enum RadioMessage {
     preto = 24635,
     message1 = 49434
 }
-radio.onReceivedMessage(RadioMessage.verde, function () {
-    // Preto e verde
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 57 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) == 255 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) < 38)) {
-        basic.showString("v")
-        basic.clearScreen()
-    }
-})
-// Significa que no 4, o sensor RGB esta detectando a cor branca a partir de parâmetros que definimos.
-radio.onReceivedMessage(RadioMessage.branco, function () {
+basic.forever(function () {
     // Detectação de qual cor o sensor RGB esta vendo no 3. Nesse caso, vai detectar a cor preta. Com o resultado final de branco no 4 e preto no 3.
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 36 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) < 85 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) == 255)) {
-        // Execução de girar em torno do eixo para se alinhar a tragetória que estará na forma de uma linha preta.
-        servos.P1.run(40)
-        servos.P2.run(40)
-        basic.showLeds(`
-            # # . . .
-            # # . . .
-            # # . . .
-            # # . . .
-            # # . . .
-            `)
-        basic.clearScreen()
-    }
-    // Detectação de qual cor o sensor RGB esta vendo no 3. Nesse caso, vai detectar a cor branca. Com o resultado final de branco no 4 e branco no 3.
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) >= 36 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) >= 85 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) == 255)) {
-        // Execução de ir reto para continuar a tragetória que estará na forma de uma linha preta.
-        servos.P1.run(15)
-        servos.P2.run(-15)
-        // String para que possamos entender o que o robô está fazendo nas execuções da programação.
-        basic.showString("b")
-        basic.clearScreen()
-    }
-    // Detectação de qual cor o sensor RGB esta vendo no 3. Nesse caso, vai detectar a cor branca. Com o resultado final de branco no 4 e branco no 3.
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) >= 36 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) >= 85 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) == 255)) {
-        basic.pause(100)
-        // Preto e verde
-        if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 57 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) == 255 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) < 38)) {
-            // Execução de girar em torno do eixo para se alinhar a tragetória que estará na forma de uma linha preta.
-            servos.P1.run(40)
-            servos.P2.run(40)
-            basic.showLeds(`
-                . # # . .
-                . # # . .
-                . # # . .
-                . # # . .
-                . # # . .
-                `)
-            basic.clearScreen()
+    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 120 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 8) {
+        if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) < 120 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 8) {
+            if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 200 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 200) {
+                basic.showIcon(IconNames.Yes)
+            }
         }
     }
 })
-// Significa que no 4, o sensor RGB esta detectando a cor preta a partir de parâmetros que definimos.
-radio.onReceivedMessage(RadioMessage.preto, function () {
-    // Detectação de qual cor o sensor RGB esta vendo no 3. Nesse caso, vai detectar a cor preta. Com o resultado final de preto no 4 e preto no 3.
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 36 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) < 85 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) == 255)) {
-        // Execução de ir reto para continuar a tragetória que estará na forma de uma linha preta.
-        servos.P1.run(15)
-        servos.P2.run(-15)
-        // String para que possamos entender o que o robô está fazendo nas execuções da programação.
-        basic.showString("p")
-        basic.clearScreen()
-    }
-    // Detectação de qual cor o sensor RGB esta vendo no 3. Nesse caso, vai detectar a cor braca. Com o resultado final de preto no 4 e branco no 3.
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) >= 36 && (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) >= 85 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) == 255)) {
-        // Execução de girar em torno do eixo para se alinhar a tragetória que estará na forma de uma linha preta.
-        servos.P1.run(-40)
-        servos.P2.run(-40)
-        // String para que possamos entender o que o robô está fazendo nas execuções da programação.
-        basic.showString("2")
-        basic.clearScreen()
+basic.forever(function () {
+    // Detectação de qual cor o sensor RGB esta vendo no 3. Nesse caso, vai detectar a cor preta. Com o resultado final de branco no 4 e preto no 3.
+    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 100 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 55) {
+        if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) < 73 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 50) {
+            if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 200 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 200) {
+                basic.showIcon(IconNames.No)
+            }
+        }
     }
 })
-// Definição do canal de cominicação entre os BBC's Micro:Bit's.
-radio.setGroup(5)
-// Número que indentifica o Micro:Bit.
-basic.showNumber(3)
+basic.forever(function () {
+    // Detectação de qual cor o sensor RGB esta vendo no 3. Nesse caso, vai detectar a cor preta. Com o resultado final de branco no 4 e preto no 3.
+    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 90 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 23) {
+        if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 200 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 200) {
+            if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) < 100 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 28) {
+                basic.showIcon(IconNames.House)
+            }
+        }
+    }
+})
