1f6c39252871dfbac8f62719d73c3c447188b949
Mobtrucky <128086337+mobtrucky@users.noreply.github.com>
2023-05-24 15:43:30 -0300

    novo padrão padrão de cores

--- main.ts
+++ main.ts
@@ -5,31 +5,31 @@ enum RadioMessage {
     message1 = 49434
 }
 basic.forever(function () {
-    // Detectação de qual cor o sensor RGB esta vendo no 3. Nesse caso, vai detectar a cor preta. Com o resultado final de branco no 4 e preto no 3.
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 120 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 8) {
-        if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) < 120 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 8) {
-            if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 200 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 200) {
-                basic.showIcon(IconNames.Yes)
+    // branco
+    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) >= 70 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) <= 255) {
+        if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) >= 70 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) <= 255) {
+            if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 250) {
+                basic.showString("B")
             }
         }
     }
 })
 basic.forever(function () {
     // Detectação de qual cor o sensor RGB esta vendo no 3. Nesse caso, vai detectar a cor preta. Com o resultado final de branco no 4 e preto no 3.
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 100 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 55) {
-        if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) < 73 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 50) {
-            if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 200 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 200) {
-                basic.showIcon(IconNames.No)
+    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) >= 5 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) <= 25) {
+        if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) >= 5 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) <= 25) {
+            if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 250) {
+                basic.showString("P")
             }
         }
     }
 })
 basic.forever(function () {
     // Detectação de qual cor o sensor RGB esta vendo no 3. Nesse caso, vai detectar a cor preta. Com o resultado final de branco no 4 e preto no 3.
-    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) < 90 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) > 23) {
-        if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 200 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 200) {
-            if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) < 100 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) > 28) {
-                basic.showIcon(IconNames.House)
+    if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) >= 12 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueR) <= 60) {
+        if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueG) > 250) {
+            if (Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) >= 15 && Module_World_Color.GetRGBValue(Module_World_Color.enGetRGB.GetValueB) <= 60) {
+                basic.showString("V")
             }
         }
     }
